794d9c33cee50284c44782d57b2f115a56ef9e74
sviatletsiuk-blip <sviatletsiuk@gmail.com>
2026-05-23 14:46:36 +0300

    fix(render): add src/index.js entrypoint for Render deployment

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const app = require('./server');
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server running on port ${PORT}`);
+});
